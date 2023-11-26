b89ce6f085f234307f2643af0683608022abb172
MikeTV24 <lurevera100@gmail.com>
2023-11-26 11:15:31 +0800

    Initial commit

---
+++ MovieStoreMvc/Models/Domain/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreMvc.Models.Domain
+{
+    public class Genre
+    {
+        internal IFormFile ImageFile;
+
+        public int Id { get; set; }
+        [Required]
+        public string? GenreName { get; set; }
+    }
+}
---
+++ MovieStoreMvc/Repositories/Abstract/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieStoreMvc.Repositories.Abstract
+{
+    public interface IFileService
+    {
+        public Tuple<int, string> SaveImage(IFormFile imageFile);
+        public bool DeleteImage(string imageFileName);
+        object SaveImage(object imageFile);
+    }
+}
